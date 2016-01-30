8f31858abf3dfe06f6fac455cf99248df5ad89fc
Katri Selonen <katri.selonen@aalto.fi>
2016-01-30 13:52:29 +0200

    Menu and settings scenes with button scripts

---
+++ Assets/Scripts/SettingsButtonController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SettingsButtonController : MonoBehaviour {
+
+	public Text soundOnText;
+
+	void Start () {
+		if (AudioListener.volume == 0) {
+			soundOnText.text = "Sound: off";
+		} else {
+			soundOnText.text = "Sound: on";
+		}
+	}
+		
+	public void ClickSound () {
+		if (AudioListener.volume == 0) {
+			AudioListener.volume = 1;
+			soundOnText.text = "Sound: on";
+		} else {
+			AudioListener.volume = 0;
+			soundOnText.text = "Sound: off";
+		}
+	}
+
+	public void ClickReset () {
+		Debug.Log ("Reset clicked");
+	}
+
+	public void ClickUnlock () {
+		Debug.Log ("Unlock clicked");
+	}
+
+	public void ClickBack () {
+		SceneManager.LoadScene ("menu");
+	}
+}	
